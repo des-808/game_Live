ac435eb245c6f1ba20cbe7747665e11fabd239f1
Sergey <94394415+des-808@users.noreply.github.com>
2023-03-25 11:09:36 +0300

    Добавьте файлы проекта.

---
+++ game_Live/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace game_Live
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            splitContainer1 = new SplitContainer();
+            btnStop = new Button();
+            btnStart = new Button();
+            numDensity = new NumericUpDown();
+            label2 = new Label();
+            numResolution = new NumericUpDown();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numDensity).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numResolution).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.BorderStyle = BorderStyle.Fixed3D;
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.FixedPanel = FixedPanel.Panel1;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Margin = new Padding(3, 2, 3, 2);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(btnStop);
+            splitContainer1.Panel1.Controls.Add(btnStart);
+            splitContainer1.Panel1.Controls.Add(numDensity);
+            splitContainer1.Panel1.Controls.Add(label2);
+            splitContainer1.Panel1.Controls.Add(numResolution);
+            splitContainer1.Panel1.Controls.Add(label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(pictureBox1);
+            splitContainer1.Size = new Size(741, 369);
+            splitContainer1.SplitterDistance = 142;
+            splitContainer1.TabIndex = 0;
+            // 
+            // btnStop
+            // 
+            btnStop.Font = new Font("Segoe Print", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnStop.Location = new Point(10, 188);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(113, 34);
+            btnStop.TabIndex = 5;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Font = new Font("Segoe Print", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnStart.Location = new Point(10, 146);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(113, 36);
+            btnStart.TabIndex = 4;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // numDensity
+            // 
+            numDensity.Location = new Point(10, 98);
+            numDensity.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
+            numDensity.Name = "numDensity";
+            numDensity.Size = new Size(120, 23);
+            numDensity.TabIndex = 3;
+            numDensity.TextAlign = HorizontalAlignment.Right;
+            numDensity.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe Print", 10F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(10, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Density";
+            // 
+            // numResolution
+            // 
+            numResolution.Location = new Point(10, 34);
+            numResolution.Maximum = new decimal(new int[] { 25, 0, 0, 0 });
+            numResolution.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numResolution.Name = "numResolution";
+            numResolution.Size = new Size(120, 23);
+            numResolution.TabIndex = 1;
+            numResolution.TextAlign = HorizontalAlignment.Right;
+            numResolution.Value = new decimal(new int[] { 25, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Print", 10F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(10, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Resolution";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(591, 365);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(741, 369);
+            Controls.Add(splitContainer1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numDensity).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numResolution).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Button btnStop;
+        private Button btnStart;
+        private NumericUpDown numDensity;
+        private Label label2;
+        private NumericUpDown numResolution;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ game_Live/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System.Globalization;
+
+namespace game_Live
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+        private bool[,] field;
+        private int rows;
+        private int cols;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void StartGame()
+        {
+            if (timer1.Enabled) { return; }
+            numResolution.Enabled = false;
+            numDensity.Enabled = false;
+
+            resolution = (int)numResolution.Value;
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+            field = new bool[rows, cols];
+
+            Random rnd = new Random();
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    field[i, j] = rnd.Next((int)numDensity.Value) == 0;
+                }
+            }
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+            //graphics.FillRectangle(Brushes.Crimson, 0, 0, resolution, resolution);
+        }
+
+        private void NextGeneration()
+        {
+            graphics.Clear(Color.Black);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (field[i, j])
+                    {
+                        graphics.FillRectangle(Brushes.Crimson, i * resolution, j * resolution, resolution, resolution);
+                    }
+                }
+            }
+            pictureBox1.Refresh();
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NextGeneration();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
